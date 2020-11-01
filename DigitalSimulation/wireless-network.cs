5b6c389d72aefa525980220ea12f9f9aca4ea65f
Dawid Janiszewski <dawiz147@gmail.com>
2020-11-01 22:09:43 +0100

    base classes were created

---
+++ DigitalSimulation/wireless-network.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSimulation
+{
+    public class WirelessNetwork
+    {
+        private static int id_package_ = -1;
+
+        public void GenerateNewIdForPackage()
+        {
+            id_package_++;
+        }
+        public int ID
+        {
+            get => id_package_;
+        }
+        public WirelessNetwork()
+        {
+
+        }
+    }
+}
--- DigitalSimulationTest/UnitTest1.cs
+++ DigitalSimulationTest/UnitTest1.cs
@@ -1,13 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using DigitalSimulation;
 namespace DigitalSimulationTest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGenerateIdForPackage()
         {
+            WirelessNetwork network = new DigitalSimulation.WirelessNetwork();
+            network.GenerateNewIdForPackage();
+            Assert.AreEqual(network.ID, 0);
         }
     }
 }
